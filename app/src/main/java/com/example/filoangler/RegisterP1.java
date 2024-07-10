a9252387e5087e5d6bfc02d1c7c482bfb10e5e8a
Kurt <kataquis.176764@lipa.sti.edu.ph>
2024-07-10 14:52:20 +0800

    Updated modules and added a Register Phase 1

---
+++ app/src/main/java/com/example/filoangler/RegisterP1.java
@@ -0,0 +1,14 @@
+package com.example.filoangler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterP1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_p1);
+    }
+}
