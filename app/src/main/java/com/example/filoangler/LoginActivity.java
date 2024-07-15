f6de689df8f6f3adb9e435cc558d1d23c6f8839d
Kurt <kataquis.176764@lipa.sti.edu.ph>
2024-07-15 08:28:40 +0800

    Updated modules, added simple backend to login module

--- app/src/main/java/com/example/filoangler/LoginActivity.java
+++ app/src/main/java/com/example/filoangler/LoginActivity.java
@@ -2,14 +2,68 @@ package com.example.filoangler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Elements
+    EditText txtEmail;
+    EditText txtPassword;
+
+    Button btnForgotPassword;
+    Button btnCreateAccount;
+    Button btnLogin;
+    Button btnGoogle;
+    Button btnOfflineMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //ForgotPassword Button
+        btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.ChangeIntent(LoginActivity.this, ForgotPassword.class);
+            }
+        });
+
+        //CreateAccount Button
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.ChangeIntent(LoginActivity.this, RegisterP1.class);
+            }
+        });
+
+        //Login Button
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Google Login Button
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Offline mode Button
+        btnOfflineMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/filoangler/Utils.java
@@ -0,0 +1,13 @@
+package com.example.filoangler;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class Utils {
+    public static void ChangeIntent(Context MainIntent, Class GoTo){
+        Intent intent = new Intent(MainIntent, GoTo);
+        MainIntent.startActivity(intent);
+    }
+}
