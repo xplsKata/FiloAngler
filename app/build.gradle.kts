3f1328f23e45ca5e9a3ad335c25cf5d4c2f98a5c
Kurt <kataquis.176764@lipa.sti.edu.ph>
2024-09-23 08:59:57 +0800

    updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -115,7 +116,7 @@ dependencies {
     implementation ("com.squareup.picasso:picasso:2.8")
 
     //Google Maps
-    implementation ("com.google.android.gms:play-services-maps:19.0.0")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.android.gms:play-services-location:21.3.0")
 
     testImplementation("junit:junit:4.13.2")
---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.8")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+
+    }
+    /*repositories{
+        google()
+        mavenCentral()
+    }*/
+}
+
+allprojects {
+    /*repositories {
+        google()
+        mavenCentral()
+    }*/
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+}
