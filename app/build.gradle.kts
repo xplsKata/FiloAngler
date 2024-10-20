25659a5590b2450ea5667054bd49a761dbf2e7f4
Kurt <kataquis.176764@lipa.sti.edu.ph>
2024-10-20 15:51:40 +0800

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
