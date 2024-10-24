4a9ea64bbdd7d94b857aa65636b2b8d2fd68d826
Kurt <kataquis.176764@lipa.sti.edu.ph>
2024-10-24 08:20:56 +0800

    added to handle multiple images of post

---
+++ app/src/main/java/com/example/filoangler/Adapter/ImagePagerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.filoangler.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.filoangler.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImagePagerAdapter extends RecyclerView.Adapter<ImagePagerAdapter.ImageViewHolder> {
+    private Context context;
+    private List<String> imageUrls;
+
+    public ImagePagerAdapter(Context context, List<String> imageUrls) {
+        this.context = context;
+        this.imageUrls = imageUrls;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post_image, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        String imageUrl = imageUrls.get(position);
+        Picasso.get().load(imageUrl).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageUrls != null ? imageUrls.size() : 0;
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imgPostImage);
+        }
+    }
+}
