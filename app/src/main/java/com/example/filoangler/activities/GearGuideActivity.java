5fb0507a5d4ce03eb6d9537235a0d3d4cfe0f269
Kurt <kataquis.176764@lipa.sti.edu.ph>
2024-11-22 11:17:20 +0800

    added base file

---
+++ app/src/main/java/com/example/filoangler/activities/GearGuideActivity.java
@@ -0,0 +1,17 @@
+package com.example.filoangler.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.filoangler.R;
+
+public class GearGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gear_guide);
+
+    }
+}
---
+++ app/src/main/java/com/example/filoangler/fragments/GearKnowledgeFragment.java
@@ -0,0 +1,21 @@
+package com.example.filoangler.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.filoangler.R;
+
+public class GearKnowledgeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gear_knowledge, container, false);
+
+        return view;
+    }
+}
